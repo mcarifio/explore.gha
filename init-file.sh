102fa9c9c57cc29df7245bf152486958bfc61952
Mike Carifio <mcarifio@ciq.com>
2025-05-06 07:21:01 -0400

    push

--- init-file.sh
+++ init-file.sh
@@ -1,7 +1,10 @@
-f.e() { [[ type -t ${1:?'expecting a function name'} = function ]]; }
+set -x
+
+f.e() { [[ $(type -t ${1:?'expecting a function name'}) = function ]]; }
 f.x() { for f in "$@"; do f.e ${f} && export -f ${f}; done; }
 f.x f.x f.e
 
 f.const() { eval $(printf '%s() ( echo '%q'; )' ${1:?'expecting a function name'} ${2:?'expecting a value'}); f.x $1; }
 f.x f.const
-f.const home $(realpath -q ${BASH_SOURCE})
+f.const home "$(realpath -Lq ${BASH_SOURCE})"
+
