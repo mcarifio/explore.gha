de44e5db1ee106042bb44d297837b8539207b4fc
Mike Carifio <mike@carif.io>
2025-05-06 08:00:29 -0400

    push

--- bash_env.sh
+++ bash_env.sh
@@ -1,4 +1,4 @@
-set -x
+# set -x
 
 f.e() { [[ $(type -t ${1:?'expecting a function name'}) = function ]]; }
 f.x() { for f in "$@"; do f.e ${f} && export -f ${f}; done; }
@@ -6,9 +6,9 @@ f.x f.e f.x
 
 f.const() { eval $(printf '%s() ( echo '%q'; )' ${1:?'expecting a function name'} ${2:?'expecting a value'}); f.x $1; }
 f.x f.const
-f.const build.root "$(realpath -Lq ${BASH_SOURCE})"
+f.const build.root "$(dirname $(realpath -Lq ${BASH_SOURCE}))"
 
 jq() ( command ${FUNCNAME} --color-output "$@"; )
 f.x jq
-set +x
+# set +x
 
